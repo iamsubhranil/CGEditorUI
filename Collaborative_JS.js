dc5392deaed730098c72591312fa840399c2e9ba
anirban-github790 <alterani.mail@gmail.com>
2021-07-19 21:38:57 +0530

    update

--- Collaborative_JS.js
+++ Collaborative_JS.js
@@ -397,7 +397,7 @@ let downloadfile = () => {
 	
 	// Convert the text to BLOB.
 	const textToBLOB = new Blob([data], { type: 'text/plain' });
-	const sFileName = 'formData.txt';	   // The file to save the data.
+	const sFileName = 'downloadtext.txt';	   // The file to save the data.
 
 	let newLink = document.createElement("a");
 	newLink.download = sFileName;
